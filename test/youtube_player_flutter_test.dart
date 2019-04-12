f54e24a75b86c7121744809101332d9982cd48b0
sarbagya <sarbagyastha@gmail.com>
2019-04-12 21:29:11 +0545

    CI added.

--- test/youtube_player_flutter_test.dart
+++ test/youtube_player_flutter_test.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
+void main() {
+  testWidgets(
+    'show player',
+    (WidgetTester tester) async {
+      BuildContext context;
+      await tester.pumpWidget(
+        YoutubePlayer(
+          key: ValueKey("sarbagya"),
+          context: context,
+          videoId: "BBAyRBTfsOU",
+          autoPlay: true,
+          width: 300,
+          aspectRatio: 16 / 9,
+          controlsTimeOut: Duration(seconds: 2),
+          hideControls: false,
+          onPlayerInitialized: (controller) {},
+          progressColors: ProgressColors(
+            handleColor: Colors.red,
+            backgroundColor: Colors.red,
+            bufferedColor: Colors.red,
+            playedColor: Colors.red,
+          ),
+          showVideoProgressIndicator: true,
+          videoProgressIndicatorColor: Colors.red,
+        ),
+      );
+      expect(find.byKey(ValueKey("sarbagya")), findsOneWidget);
+    },
+  );
+}
