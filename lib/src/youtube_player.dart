097f857e61191427f8d6afeb672ba1ddc2a189cb
tvc12 <meomeocf98@gmail.com>
2019-10-06 10:48:40 +0700

    Fix url was call null at function convertUrlToId

--- lib/src/youtube_player.dart
+++ lib/src/youtube_player.dart
@@ -114,8 +114,8 @@ class YoutubePlayer extends StatefulWidget {
 
   /// Converts fully qualified YouTube Url to video id.
   static String convertUrlToId(String url, [bool trimWhitespaces = true]) {
+    if (url == null || url.isEmpty) return null;
     if (!url.contains("http") && (url.length == 11)) return url;
-    if (url == null || url.length == 0) return null;
     if (trimWhitespaces) url = url.trim();
 
     for (var exp in [
