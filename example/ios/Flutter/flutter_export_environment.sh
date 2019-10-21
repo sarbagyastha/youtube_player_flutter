47d210694e7e9238bab1e05557d128e7462fa5db
sarbagyastha <mail@sarbagyastha.com.np>
2019-10-21 11:00:06 +0545

    Ignored unnecessary files.

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/sarbagya/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/sarbagya/development/plugins/youtube_player_flutter/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Development\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Development\plugins\youtube_player_flutter\example"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/sarbagya/development/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Development\flutter\bin\cache\artifacts\engine\ios"
