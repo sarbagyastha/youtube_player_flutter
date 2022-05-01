ee362a2bea6adaff5a5c9379a12c205afd065eba
atul garg <gargat@gmail.com>
2022-05-01 08:40:25 -0400

    fix(double volume): made double volume equal 200

--- packages/youtube_player_flutter/example/lib/main.dart
+++ packages/youtube_player_flutter/example/lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   late PlayerState _playerState;
   late YoutubeMetaData _videoMetaData;
-  double _volume = 100;
+  double _volume = 200;
   bool _muted = false;
   bool _isPlayerReady = false;
 
