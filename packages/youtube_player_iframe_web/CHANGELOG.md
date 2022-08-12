b372657aa3757129dc8b11450cc99b18ae38ef22
sarbagyastha <sarbagyastha@gmail.com>
2022-08-12 15:21:53 +0545

    :sparkles: added `youtube_player_iframe_web` package

---
+++ packages/youtube_player_iframe_web/CHANGELOG.md
@@ -0,0 +1,2 @@
+## 1.0.0
+- Web implementation of webview_flutter plugin tailored for using with youtube_player_iframe package.
---
+++ packages/youtube_player_iframe_web/lib/youtube_player_iframe_web.dart
@@ -0,0 +1,7 @@
+library youtube_player_iframe_web;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
