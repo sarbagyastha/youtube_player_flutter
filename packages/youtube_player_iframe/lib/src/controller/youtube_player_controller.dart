ace3df45f69b9b92ef2da51049b66a033cf2706c
sarbagyastha <sarbagyastha@gmail.com>
2022-12-24 19:13:24 +0545

    :bug: minor fix

--- packages/youtube_player_iframe/lib/src/controller/youtube_player_controller.dart
+++ packages/youtube_player_iframe/lib/src/controller/youtube_player_controller.dart
@@ -671,11 +671,12 @@ class YoutubeVideoState {
   factory YoutubeVideoState.fromJson(String json) {
     final state = jsonDecode(json);
     final currentTime = state['currentTime'] as num? ?? 0;
+    final loadedFraction = state['loadedFraction'] as num? ?? 0;
     final positionInMs = (currentTime * 1000).truncate();
 
     return YoutubeVideoState(
       position: Duration(milliseconds: positionInMs),
-      loadedFraction: state['loadedFraction'],
+      loadedFraction: loadedFraction.toDouble(),
     );
   }
 
