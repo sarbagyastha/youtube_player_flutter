d08b39a674eace7b986f21ec8ab416673948e820
abhishekdixit <abhishek.dixit@gytworkz.com>
2023-01-06 12:05:12 +0530

    Added Total Duration feature in youtube_player_flutter package

--- packages/youtube_player_flutter/lib/src/widgets/duration_widgets.dart
+++ packages/youtube_player_flutter/lib/src/widgets/duration_widgets.dart
@@ -121,3 +121,60 @@ class _RemainingDurationState extends State<RemainingDuration> {
     );
   }
 }
+
+/// A widget which displays the Total duration of the video.
+class TotalDuration extends StatefulWidget {
+  /// Overrides the default [YoutubePlayerController].
+  final YoutubePlayerController? controller;
+
+  /// Creates [TotalDuration] widget.
+  const TotalDuration({this.controller});
+
+  @override
+  State<TotalDuration> createState() => _TotalDurationState();
+}
+
+class _TotalDurationState extends State<TotalDuration> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final controller = YoutubePlayerController.of(context);
+    if (controller == null) {
+      assert(
+        controller != null,
+        '\n\nNo controller could be found in the provided context.\n\n'
+        'Try passing the controller explicitly.',
+      );
+      _controller = controller!;
+    } else {
+      _controller = controller;
+    }
+    _controller.removeListener(listener);
+    _controller.addListener(listener);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(listener);
+    super.dispose();
+  }
+
+  void listener() {
+    if (mounted) setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      durationFormatter(
+        (_controller.metadata.duration.inMilliseconds),
+      ),
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 12.0,
+      ),
+    );
+  }
+}
