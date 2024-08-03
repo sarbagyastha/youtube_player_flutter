b33cc7eea9605f081630aca1cf760dac4b595b30
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2024-08-03 15:24:17 +0545

    :art: used go router for routing

--- packages/youtube_player_iframe/example/lib/pages/video_list_page.dart
+++ packages/youtube_player_iframe/example/lib/pages/video_list_page.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 Sarbagya Dhaubanjar. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
