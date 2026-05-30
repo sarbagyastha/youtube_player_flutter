3b28bce301adf19cf23a4462e48242d7bf8504ff
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2026-05-30 21:25:28 +0545

    updated example app icon

--- packages/youtube_player_flutter/example/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:ypf_example/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.text('YouTube Player Flutter'), findsOneWidget);
-  });
-}
